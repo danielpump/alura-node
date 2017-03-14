eae7e10976de00cfdc64aadb8fa41a0d88571bf2
Daniel <mzp_daniel@uolinc.com>
2017-03-14 18:37:31 -0300

    Melhorando a forma como as conexões são distribuidas na aplicação

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -2,13 +2,23 @@
 module.exports = function(app){	
     app.get('/produtos',function(req,res){    	
          var connection = app.infra.connectionFactory();
+         var produtosBanco = app.infra.produtosBanco(connection);
 
-         var produtosBanco = app.infra.produtosBanco;
-
-         produtosBanco.lista(connection, function(err,results){
+         produtosBanco.lista(function(err,results){
             res.render('produtos/lista',{lista:results});
         });
         connection.end();
 	});	
+
+	app.get('produtos/remove',function(){
+        var connection = app.infra.connectionFactory();
+        var produtosBanco = app.infra.produtosBanco(connection);
+        
+        var produto = produtosBanco.carrega(id,callback);
+        if(produto){
+            produtosBanco.remove(produto,callback);
+        }
+
+    });
 }
 
